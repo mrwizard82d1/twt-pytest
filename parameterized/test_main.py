dabc50de600c86a6718f98a355212bbf6200fe1d
Larry Jones <mrwizard82d1@gmail.com>
2025-02-25 16:28:05 -0600

    Parametrizing a test

---
+++ parameterized/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from main import is_prime
+
+
+@pytest.mark.parametrize('num, expected', [(1, False),
+                                           (2, True),
+                                           (3, True),
+                                           (4, False),
+                                           (17, True),
+                                           (18, False),
+                                           (19, True),
+                                           (20, False)
+                                           ])
+def test_is_prime(num, expected):
+    assert is_prime(num) == expected
