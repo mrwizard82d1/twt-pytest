4534396a427ec57d85e11d2a1a53d6697d34c226
Larry Jones <mrwizard82d1@gmail.com>
2025-02-25 16:05:34 -0600

    BROKE - An error occurs **without** a fixture

--- fixtures/test_main.py
+++ fixtures/test_main.py
@@ -2,21 +2,26 @@ import pytest
 from main import UserManager
 
 
-@pytest.fixture
-def user_manager():
-    """
-    Creates a fresh instance of `UserManager` before each test.
-    :return: A fresh instance of `UserManager`
-    """
-    return UserManager()
+# Replace fixture with "global variable"
+# @pytest.fixture
+# def user_manager():
+#     """
+#     Creates a fresh instance of `UserManager` before each test.
+#     :return: A fresh instance of `UserManager`
+#     """
+#     return UserManager()
+user_manager = UserManager()
 
 
-def test_add_user(user_manager):
+def test_add_user():
     assert user_manager.add_user('john doe', 'john@example.com')
     assert user_manager.get_user('john doe') ==  'john@example.com'
 
 
-def test_add_duplicate_user(user_manager):
+def test_add_duplicate_user():
+    # Now the first line raises a `ValueError` because user, 'john doe',
+    # was added by the previous test. (Tests run from top to bottom
+    # of file.)
     user_manager.add_user('john doe', 'john@example.com')
     with pytest.raises(ValueError):
         user_manager.add_user('john doe', 'john@example.com')
