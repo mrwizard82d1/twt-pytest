929f9dc889ef704ab4cceeab19c3149a453208a4
Larry Jones <mrwizard82d1@gmail.com>
2025-02-25 17:02:25 -0600

    Mocking a (sqlite3) database and a cursor
    This example shows how to mock return values from other mocks.

---
+++ mocks/db.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+
+def save_user(name, age):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (name, age) VALUES (?, ?)', (name, age))
+    conn.commit()
+    conn.close()
---
+++ mocks/test_db.py
@@ -0,0 +1,16 @@
+from db import save_user
+
+
+def test_save_user(mocker):
+    # Mocking the database **avoids** creating a real database - even sqlite3.
+    # We mock the connection
+    mock_conn = mocker.patch('sqlite3.connect')
+    # We then mock the call to `cursor` on the "connection"
+    mock_cursor = mock_conn.return_value.cursor.return_value
+
+    save_user('Alice', 30)
+
+    mock_conn.assert_called_once_with('users.db')
+    mock_cursor.execute.assert_called_once_with(
+        'INSERT INTO users (name, age) VALUES (?, ?)', ('Alice', 30)
+    )
