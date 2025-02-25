9b280997fb8ca030ae01843c01d8b9b893f5b66e
Larry Jones <mrwizard82d1@gmail.com>
2025-02-25 16:07:21 -0600

    Revert "BROKE - Error occurs **without** fixture"
    This reverts commit 4534396a427ec57d85e11d2a1a53d6697d34c226.

--- fixtures/test_main.py
+++ fixtures/test_main.py
@@ -2,26 +2,21 @@ import pytest
 from main import UserManager
 
 
-# Replace fixture with "global variable"
-# @pytest.fixture
-# def user_manager():
-#     """
-#     Creates a fresh instance of `UserManager` before each test.
-#     :return: A fresh instance of `UserManager`
-#     """
-#     return UserManager()
-user_manager = UserManager()
+@pytest.fixture
+def user_manager():
+    """
+    Creates a fresh instance of `UserManager` before each test.
+    :return: A fresh instance of `UserManager`
+    """
+    return UserManager()
 
 
-def test_add_user():
+def test_add_user(user_manager):
     assert user_manager.add_user('john doe', 'john@example.com')
     assert user_manager.get_user('john doe') ==  'john@example.com'
 
 
-def test_add_duplicate_user():
-    # Now the first line raises a `ValueError` because user, 'john doe',
-    # was added by the previous test. (Tests run from top to bottom
-    # of file.)
+def test_add_duplicate_user(user_manager):
     user_manager.add_user('john doe', 'john@example.com')
     with pytest.raises(ValueError):
         user_manager.add_user('john doe', 'john@example.com')
