be9a3f4cf3f36b2a47ee0fe43a7dd14e3d495bf2
Larry Jones <mrwizard82d1@gmail.com>
2025-02-25 16:29:26 -0600

    Revert "BROKE - Introduce a failing test"
    This reverts commit 8fe3c5ce8fd27105d4279c139195979502be4c02.

--- parameterized/test_main.py
+++ parameterized/test_main.py
@@ -7,7 +7,7 @@ from main import is_prime
                                            (3, True),
                                            (4, False),
                                            (17, True),
-                                           (18, True),
+                                           (18, False),
                                            (19, True),
                                            (20, False)
                                            ])
